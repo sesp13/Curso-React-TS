4dadeda34263c34b37a0276f63cd4a5384d6dde8
sesp13 <sespinosarte99@gmail.com>
2024-09-30 23:09:34 -0500

    create project - currencies

---
+++ cotizador-cryptos/src/App.tsx
@@ -0,0 +1,16 @@
+import { CryptoSearchForm } from './components/CryptoSearchForm';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="app-title">
+        Cotizador de <span>Criptomonedas</span>
+      </h1>
+      <div className="content">
+        <CryptoSearchForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ cotizador-cryptos/src/components/CryptoSearchForm.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { currencies } from '../data';
+
+export const CryptoSearchForm = () => {
+  return (
+    <form className="form">
+      <div className="field">
+        <label htmlFor="currency">Moneda: </label>
+        <select name="currency" id="currency">
+          <option value="">-- Seleccione --</option>
+          {currencies.map((currency) => (
+            <option key={currency.code} value={currency.code}>
+              {currency.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="field">
+        <label htmlFor="cryptocurrency">Criptomoneda: </label>
+        <select name="cryptocurrency" id="cryptocurrency">
+          <option value="">-- Seleccione --</option>
+        </select>
+      </div>
+
+      <input type="submit" value="Cotizar" />
+    </form>
+  );
+};
