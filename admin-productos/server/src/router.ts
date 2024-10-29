71827aecc19437b642d4d9d173568366e3830bd6
sesp13 <sespinosarte99@gmail.com>
2024-10-28 23:28:57 -0500

    start document endpoints with swagger

--- admin-productos/server/src/router.ts
+++ admin-productos/server/src/router.ts
@@ -11,12 +11,84 @@ import {
 import { Router } from 'express';
 import { handleInputErrors } from './middlewares';
 
+// Base /api/products
 const router = Router();
 
-// Base /api/products
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Product:
+ *      type: object
+ *      properties: 
+ *        id: 
+ *          type: integer
+ *          description: The product ID
+ *          example: 1
+ *        name: 
+ *          type: string
+ *          description: The product name
+ *          example: Monitor curvo de 49 pulgadas
+ *        price: 
+ *          type: number
+ *          description: The product price
+ *          example: 300
+ *        availability: 
+ *          type: boolean
+ *          description: The product availability
+ *          example: true
+ */
 
+
+
+/**
+ * @swagger
+ * /api/products:
+ *  get: 
+ *    summary: Get a list of products
+ *    tags: 
+ *      - Products
+ *    description: Return a list of products
+ *    responses: 
+ *      200: 
+ *        description: Succesful response
+ *        content: 
+ *          application/json:
+ *            schema: 
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Product'
+ */
 router.get('/', getProducts);
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  get:
+ *    summary: Get a product by ID
+ *    tags:
+ *      - Products
+ *    description: Return a product based on it's unique ID
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      description: The ID of the product to retreive
+ *      required: true
+ *      schema:
+ *        type: integer
+ *    responses: 
+ *      200:
+ *        description: Succesful response
+ *        content:
+ *          application/json:
+ *            schema: 
+ *              $ref: '#/components/schemas/Product'
+ *      404:
+ *        description: Not Found
+ *      400:
+ *        description: Bad request, invalid ID
+ * 
+ */
 router.get(
   '/:id',
   param('id').isInt().withMessage('id no válido'),
