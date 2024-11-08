ab919de2e4f9ee7671264111cd6c5f07ee457f96
sesp13 <sespinosarte99@gmail.com>
2024-11-07 23:31:13 -0500

    add cors config and logs with morgan

--- admin-productos/server/src/server.ts
+++ admin-productos/server/src/server.ts
@@ -1,8 +1,10 @@
+import cors, { CorsOptions } from 'cors';
 import swaggerSpec, { swaggerUiOptions } from './config/swagger';
 
 import colors from 'colors';
 import db from './config/db';
 import express from 'express';
+import morgan from 'morgan';
 import productsRouter from './router';
 import swaggerUi from 'swagger-ui-express';
 
@@ -22,8 +24,25 @@ connectDB();
 
 const server = express();
 
+// Permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      // Permitir conexión
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
+
 // Habilitar lectura de json
 server.use(express.json());
+
+// Logs con Morgan
+server.use(morgan('dev'));
 
 // Routing
 server.use('/api/products', productsRouter);
