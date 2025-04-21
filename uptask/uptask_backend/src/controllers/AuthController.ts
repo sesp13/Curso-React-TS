1f534a3eee61555cb367af3727f50b032cf53aad
sesp13 <sespinosarte99@gmail.com>
2025-04-20 23:40:26 -0500

    uptask_be: generate token

--- uptask/uptask_backend/src/controllers/AuthController.ts
+++ uptask/uptask_backend/src/controllers/AuthController.ts
@@ -47,4 +47,30 @@ export class AuthController {
       return;
     }
   };
+  static confirmAccount = async (req: Request, res: Response) => {
+    try {
+      const { token } = req.body;
+      const tokenExists = await Token.findOne({ token });
+      if (!tokenExists) {
+        const error = new Error('Token no válido!');
+        res.status(401).json({ error: error.message });
+        return;
+      }
+
+      const user = await User.findById(tokenExists.user);
+      user.confirmed = true;
+
+      await Promise.allSettled([user.save(), tokenExists.deleteOne()]);
+
+      res.json({ msg: 'Cuenta confirmada correctamente!' });
+
+      return;
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        msg: 'Error inesperado contacte al administrador',
+      });
+      return;
+    }
+  };
 }
--- uptask/uptask_backend/src/routes/authRoutes.ts
+++ uptask/uptask_backend/src/routes/authRoutes.ts
@@ -22,4 +22,11 @@ router.post(
   AuthController.createAccount
 );
 
+router.post(
+  '/confirm-account',
+  body('token').notEmpty().withMessage('El Token no puede ir vacio'),
+  handleInputErrors,
+  AuthController.confirmAccount
+);
+
 export default router;
